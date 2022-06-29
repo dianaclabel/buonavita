6f97cf8871ad87ad3435a6d3bcfc880fae1ec8e4
Diana Clabel <dianaclabel08@gmail.com>
2022-06-28 23:06:13 -0500

    add imc to file

--- js/principal.js
+++ js/principal.js
@@ -1,13 +1,19 @@
 var paciente = document.querySelector("#primer-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
-
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
-
 var altura = tdAltura.textContent;
 
+var tdIMC = paciente.querySelector(".info-imc");
 var imc = peso / (altura * altura);
+tdIMC.textContent = imc;
 
-console.log(imc);
+if (peso < 0 || peso > 1000) {
+  console.log("Peso incorrecto");
+}
+
+if (altura < 0 || altura > 4.0) {
+  console.log("Altura incorrecto");
+}
