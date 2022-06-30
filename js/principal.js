b433c7ed26f36e46097e09eae8cddc2f7879259e
Diana Clabel <dianaclabel08@gmail.com>
2022-06-29 21:27:39 -0500

    agrega condicion al IMC

--- js/principal.js
+++ js/principal.js
@@ -7,13 +7,23 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdIMC = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdIMC.textContent = imc;
+
+pesosEsValido = true;
+alturaEsValida = true;
 
 if (peso < 0 || peso > 1000) {
   console.log("Peso incorrecto");
+  tdIMC.textContent = "Peso incorrecto";
+  pesosEsValido = false;
 }
 
-if (altura < 0 || altura > 4.0) {
+if (altura < 0 || altura > 3.0) {
   console.log("Altura incorrecto");
+  tdIMC.textContent = "Altura incorrecta";
+  alturaEsValida = false;
+}
+
+if (pesosEsValido && alturaEsValida) {
+  var imc = peso / (altura * altura);
+  tdIMC.textContent = imc;
 }
