0cbfa0ed6eed2a2621bfe96535da7e601fae24c2
Diana Clabel <dianaclabel08@gmail.com>
2022-07-05 22:41:06 -0500

    agrega mas informacion a la tabla

--- js/principal.js
+++ js/principal.js
@@ -2,9 +2,35 @@ var botonAdicionar = document.querySelector("#adicionar-paciente");
 
 botonAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
-
+  //captura el emento del formulario
   var form = document.querySelector("#form-adicionar");
-  console.log(form.altura);
+  //se busca y captura la infromacion dentro del formulario
+  var nombre = form.nombre.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var tabla = document.querySelector("#tabla-pacientes");
+
+  //createElemnt() nos permite crear un elemnto de HTML desde js
+  pacienteTr = document.createElement("tr");
+  nombreTd = document.createElement("td");
+  alturaTd = document.createElement("td");
+  pesoTd = document.createElement("td");
+  gorduraTd = document.createElement("td");
+  imcTd = document.createElement("td");
+
+  nombreTd.textContent = nombre;
+  alturaTd.textContent = altura;
+  pesoTd.textContent = peso;
+  gorduraTd.textContent = gordura;
+  // insertamos los td  dentro del  tr
+  pacienteTr.appendChild(nombreTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  tabla.appendChild(pacienteTr);
 });
 
 var pacientes = document.querySelectorAll(".paciente");
