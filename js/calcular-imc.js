ac2153e50c12974f3ee107038fdaeeb574735488
Diana Clabel <dianaclabel08@gmail.com>
2022-07-06 19:04:17 -0500

    separamos las funcionalidades en diferentes archivos

---
+++ js/calcular-imc.js
@@ -0,0 +1,34 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdIMC = paciente.querySelector(".info-imc");
+
+  pesosEsValido = true;
+  alturaEsValida = true;
+
+  if (peso < 0 || peso > 1000) {
+    console.log("Peso incorrecto");
+    tdIMC.textContent = "Peso incorrecto";
+    pesosEsValido = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
+
+  if (altura < 0 || altura > 3.0) {
+    console.log("Altura incorrecto");
+    tdIMC.textContent = "Altura incorrecta";
+    alturaEsValida = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
+
+  if (pesosEsValido && alturaEsValida) {
+    var imc = peso / (altura * altura);
+    tdIMC.textContent = imc.toFixed(2);
+  }
+}
