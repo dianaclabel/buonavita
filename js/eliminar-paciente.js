a0c4ac3b3d487699be750fc95e9ea910c3cf23e2
Diana Clabel <dianaclabel08@gmail.com>
2022-07-21 00:23:21 -0500

    agrega mas estilos al momento de eliminar pacientes

--- js/eliminar-paciente.js
+++ js/eliminar-paciente.js
@@ -5,8 +5,11 @@ tabla.addEventListener("dblclick", function (event) {
   //event.bubling se dispara primero a los elemntos mas internos y luegoa sus antepasados.
   //el even.target nos da info  donde sucedio el evento
   //ParentNode hace que se refiera al padre donde se selecciono.es decir "tr"
-  event.target.parentNode.remove();
+
   event.target.parentNode.classList.add("fadeOut");
+  setTimeout(function () {
+    event.target.parentNode.remove();
+  }, 500);
 });
 
 /*
