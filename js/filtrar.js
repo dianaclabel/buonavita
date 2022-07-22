cd69d3bd7ecfcb5ab9c9de90c01b80026a1fd44b
Diana Clabel <dianaclabel08@gmail.com>
2022-07-21 21:24:21 -0500

    se agrega funcionalidad para filtrar

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,9 +1,27 @@
 var campoFiltro = document.querySelector("#filtrar-tabla");
 
 campoFiltro.addEventListener("input", function () {
-  console.log("escribieron");
-  console.log(this.value);
-
   var pacientes = document.querySelectorAll(".paciente");
-  console.log(pacientes);
+
+  if (this.value.length > 0) {
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      var tdNombre = paciente.querySelector(".info-nombre");
+      var nombre = tdNombre.textContent;
+      //Es un objecto que describe el padron de elemntos , regex es usado para buscar y reemplazar
+      var expresion = new RegExp(this.value, "i");
+      //regex tiene una funcionalidad para testear si el valor se encuentra en otro  elemnto.
+      //"a" hace parte de pablo se ejecuta el else
+      if (!expresion.test(nombre)) {
+        paciente.classList.add("invisible");
+      } else {
+        paciente.classList.remove("invisible");
+      }
+    }
+  } else {
+    for (var i = 0; i < pacientes.length; i++) {
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisible");
+    }
+  }
 });
